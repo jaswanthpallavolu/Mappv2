cf28c9cf83420d3f63b87257fddddcccd831007c
Pranairaj <kaligithipranai@gmail.com>
2022-06-14 13:30:33 +0530

    Feature section

---
+++ components/homesection/FeatureItem.jsx
@@ -0,0 +1,13 @@
+import styles from "./feature.module.css";
+
+export default function featureItem(props) {
+    const { info } = props;
+
+
+    return (
+        <div className={styles.item}>
+            <h3></h3>
+            <p></p>
+        </div>
+    );
+}
---
+++ components/homesection/Features.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./feature.module.css";
+import featureItem from "./FeatureItem";
+
+function Features() {
+    const featurelist = [
+        {
+            id: "1",
+            name: "",
+            description: "",
+        },
+        {
+            id: "2",
+            name: "",
+            description: "",
+        },
+        {
+            id: "3",
+            name: "",
+            description: "",
+        },
+        {
+            id: "4",
+            name: "",
+            description: "",
+        }
+    ];
+
+    return (
+        <div className={styles.features}>
+            {featurelist.map((f, index) => (
+                <featureItem key={index} info={f} />
+            ))}
+        </div>
+    );
+
+}
